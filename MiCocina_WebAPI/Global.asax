c6ffc5421b07ffce731ee946656b7dafc917a11f
Shirou18 <104110217+Shirou18@users.noreply.github.com>
2024-06-22 15:00:57 -0600

    Add project files.

---
+++ MiCocina_WebAPI/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MiCocina_WebAPI.MvcApplication" Language="C#" %>
---
+++ MiCocina_WebAPI/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MiCocina_WebAPI.Startup))]
+namespace MiCocina_WebAPI
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
